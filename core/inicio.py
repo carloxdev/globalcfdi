962f470fb5adbb341fa0aeaf2717616228d8497a
SVR-NOMI <Administrador@SIC.local>
2016-11-23 09:56:52 -0600

    Cambios para la descargada de facturas

--- core/inicio.py
+++ core/inicio.py
@@ -10,4 +10,5 @@ from tecnology import Cfdineitor
 if __name__ == '__main__':
     run_path = os.path.abspath(os.path.join(os.getcwd(), os.pardir))
     app = Cfdineitor("PRODUCCION", run_path)
-    app.get_Invoices_ofValidCompanies()
+    # app.get_Invoices_ofValidCompanies()
+    app.get_Invoices_Company("OKTO")
