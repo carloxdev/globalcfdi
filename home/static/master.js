19437311a2859ffa53fe0deb506d0412b3bacdf6
carloxdev <carloxdev@gmail.com>
2016-12-05 22:24:44 -0600

    Se agregrego el archivo master

---
+++ home/static/master.js
@@ -0,0 +1,11 @@
+/*-----------------------------------------------*\
+            OBJETO: Pagina
+\*-----------------------------------------------*/
+
+function Pagina () {
+
+}
+Pagina.prototype.init_Alertify = function () {
+    alertify.set('notifier', 'position', 'top-right')
+    alertify.set('notifier', 'delay', 10)	
+}
