55e83bfac67b5e60e468d461baaec8af4c261416
carloxdev <carloxdev@gmail.com>
2017-02-23 23:05:40 -0600

    Cambios en pruebas

--- core/test.py
+++ core/test.py
@@ -11,4 +11,6 @@ from tecnology import Cfdineitor
 if __name__ == '__main__':
     run_path = os.path.abspath(os.path.join(os.getcwd(), os.pardir))
     app = Cfdineitor("PRODUCCION", run_path)
-    app.get_ByRange('EXSEN', date(2017, 2, 2), date(2017, 2, 2))
+    # app.get_ByRange('EXSEN', date(2017, 2, 2), date(2017, 2, 2))
+    app = Cfdineitor("PRODUCCION", run_path)
+    app.get_AllCompanies_Last3Days()
